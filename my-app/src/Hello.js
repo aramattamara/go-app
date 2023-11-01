65ed1f52c1d1fb11f6661208f07fe1d57e03679c
Tamara Lazerka <lazerkatamara@gmail.com>
2023-11-01 14:38:45 -0700

    Add AppClass and Hello

---
+++ my-app/src/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Hello.css';
+
+
+function Hello() {
+    return (
+        <>
+            <hr />
+            <h1 className="h1-green"> Hello, world!</h1>
+        </>
+    )
+}
+
+export default Hello;
