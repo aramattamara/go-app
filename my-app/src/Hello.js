ca101c52cdc8b2c705baa86029e4a7ac0ff16b8c
Tamara Lazerka <lazerkatamara@gmail.com>
2023-11-01 17:21:31 -0700

    Add toggleTrue function

--- my-app/src/Hello.js
+++ my-app/src/Hello.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {Fragment, useState} from "react";
 import './Hello.css';
 
 
-function Hello() {
+function Hello(props) {
+    const [isTrue, setIsTrue] = useState(false)
+
+    const toggleTrue = () => {
+        if (isTrue) {
+            setIsTrue(false)
+            return
+        }
+        setIsTrue(true)
+    }
+
     return (
         <>
             <hr />
-            <h1 className="h1-green"> Hello, world!</h1>
+            <h1 className="h1-green"> {props.msg}</h1>
+            <hr />
+            {isTrue &&
+                <Fragment>
+                    <p>The current value of isTrue is true</p>
+                    <hr />
+                </Fragment>
+            }
+            <hr/>
+            <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>Toggle is true</a>
         </>
     )
 }
